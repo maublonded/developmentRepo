4d9dbbe9d21aa466282224f5b485b9cf71e5d782
maublonded <mauricioht13@gmail.com>
2025-02-04 10:30:06 -0700

    combination list for checking winner

--- dev/gato/tablero.py
+++ dev/gato/tablero.py
@@ -1,20 +1,70 @@
 '''
-tablero.py: Dibuja el tablero del juego del gato
+tablero.py Draws the board of the tiktaktoe game
 '''
-def dibujo_tablero(simbolos:dict):
+import random
+def dibujar_tablero(simbolos:dict):
+ 
     print(f'''
     {simbolos['1']} | {simbolos['2']} | {simbolos['3']}
-    ----------
+    ---------
     {simbolos['4']} | {simbolos['5']} | {simbolos['6']}
-    ----------
+    ---------
     {simbolos['7']} | {simbolos['8']} | {simbolos['9']}
     ''')
-    
-if __name__ == '__main__':
+ 
+def ia(simbolos:dict):
+ 
+    '''Juega la maquina'''
+    ocupado = True
+    while ocupado is True:
+        x = random.choice(list(simbolos.keys()))
+        if simbolos[x] not in ['X','O']:
+            simbolos[x] = 'O'
+            ocupado = False
+ 
+def usuario(simbolos:dict):
+    '''Juega el usuario'''
+    lista_numeros = [str(i) for i in range(1,10)]
+    ocupado = True
+    while ocupado is True:
+        x = input('Type the number u want to place the X in: ')
+        if x in lista_numeros:
+            if simbolos[x] not in ['X','O']:
+                simbolos[x] = 'X'
+                ocupado = False
+            else:
+                print('U cant place there')
+        else:
+            print('Not a valid number')
+ 
+ 
+ 
+ 
+if __name__ =='__main__':
     numeros = [str(x) for x in range(1,10)]
-    simbolos = {x:x for x in numeros}
-    dibujo_tablero(simbolos)
-    simbolos['1'] = 'X'
-    dibujo_tablero(simbolos)
-    simbolos['5'] = 'O'
-    dibujo_tablero(simbolos)
+    dsimbolos = {x:x for x in numeros}
+    dibujar_tablero(dsimbolos)
+    ia(dsimbolos)
+    dibujar_tablero(dsimbolos)
+    usuario(dsimbolos)
+    dibujar_tablero(dsimbolos)
+
+def juego(simbolos:dict):
+    '''
+    Juego del gato
+    '''
+    lista_combinaciones = {
+            ['1','2','3'],
+            ['4','5','6'],
+            ['7','8','9'],
+            ['1','4','7'],
+            ['2','5','8'],
+            ['3','6','9'],
+    }
+
+def check_winner(simbolos:dict, combinaciones:dict):
+    '''Checa si hay un ganador'''
+    for c in combinaciones:
+        if simbolos[c[0]] == simbolos[c[1]] == simbolos[c[2]]:
+            return simbolos[combinaciones[0]]
+        return None
