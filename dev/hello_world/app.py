c2f54260642e6d87682bb11b1406557d937c7816
maublonded <mauricioht13@gmail.com>
2025-03-05 10:39:28 -0700

    completed hello world app with logs

--- dev/hello_world/app.py
+++ dev/hello_world/app.py
@@ -4,11 +4,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<html><head><title>Hello World</title></head><body><h1>Hello World</h1> <p> Ir a <a href="/about">about</a></p></body></html>'
+    return'''
+            <html>
+                <head>
+                <title>Hello World</title>
+                </head>
+                <body>
+                    <h1>Hello World</h1> 
+                    <p> Ir a <a href="/about">about</a></p>
+                </body>
+            </html>
+            '''
 
 @app.route('/about')
 def about():
-    '<html><head><title>About this page </title></head><body> Everything about this website. Back to <a href="/"> Hello World </a></body></html>'
+    return'''
+    <html>
+        <head>
+        <title>About this page </title>
+        </head>
+        <body> Everything about this website. Back to <a href="/"> Hello World </a>
+        </body>
+    </html>
+    '''
 
 if __name__ == '__main__':
     app.run(debug=True)
