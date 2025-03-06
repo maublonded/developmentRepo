9bb3494b7d8842facebb83aa96ed8ed6c3dbc606
maublonded <mauricioht13@gmail.com>
2025-03-06 10:28:33 -0700

    Searching for a book now works

--- dev/libros_web/funciones.py
+++ dev/libros_web/funciones.py
@@ -9,10 +9,20 @@ def leer_archivo_csv(archivo:str)->list:
     
 def crea_diccionario_titulos(lista:list)->dict:
     ''' Funcion que crea un diccionario con los titulos de los libros como clave y el resto de los datos como valores'''
-    return {x['titulo']:x for x in lista}
+    return {x['title']:x for x in lista}
+
+def busca_en_titulo(diccionario, palabra)-> list:
+    ''' Busca palabra en titulo de la lista de diccionarios '''
+    lista = []
+    palabra = palabra.lower()
+    for titulo, libro in diccionario.items():
+        if palabra in titulo.lower():
+            lista.append(libro)
+    return lista
 
 if __name__ == '__main__':
     archivo_csv = 'booklist2000.csv'
     lista_libros = leer_archivo_csv(archivo_csv)
     diccionario_libtos = crea_diccionario_titulos(lista_libros)
-    
+    resultado = busca_en_titulo(diccionario_libtos, 'rebels')
+    print(resultado)
