40e1670ef149f33883ca534c2514652bb9db9b55
maublonded <mauricioht13@gmail.com>
2025-03-06 10:53:25 -0700

    Creating dictionaries and Title and Author search fully functioning - :p

--- dev/libros_web/funciones.py
+++ dev/libros_web/funciones.py
@@ -1,5 +1,4 @@
 ''' Archivo con las funciones necesarias de la Aplicacion libro_web '''
-
 import csv
 
 def leer_archivo_csv(archivo:str)->list:
@@ -20,9 +19,29 @@ def busca_en_titulo(diccionario, palabra)-> list:
             lista.append(libro)
     return lista
 
+def crea_diccionario_autor(lista:list, llave:str)->dict:
+    ''' Funcion que crea un diccionario con los autores de los libros como clave y el resto de los datos como valores'''
+    return {x[llave]:x for x in lista}
+
+def busca_en_diccionario(diccionario:dict, palabra:str)-> list:
+    ''' Busca palabra en llave de la lista de diccionarios '''
+    lista = []
+    palabra = palabra.lower()
+    for llave, libro in diccionario.items():
+        if palabra in llave.lower():
+            lista.append(libro)
+    return lista
+
 if __name__ == '__main__':
     archivo_csv = 'booklist2000.csv'
     lista_libros = leer_archivo_csv(archivo_csv)
-    diccionario_libtos = crea_diccionario_titulos(lista_libros)
-    resultado = busca_en_titulo(diccionario_libtos, 'rebels')
+    diccionario_libros = crea_diccionario_titulos(lista_libros)
+    resultado = busca_en_titulo(diccionario_libros, 'rebels')
+    print()
+    print("================ Busqueda por titulo =====================")
+    print(resultado)
+    diccionario_autores = crea_diccionario_autor(lista_libros, 'author')
+    resultado = busca_en_diccionario(diccionario_autores, 'Susan')
+    print()
+    print("================ Busqueda por autor ======================")
     print(resultado)
